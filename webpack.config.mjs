3082f791561680cbc289ca438c6606bf7e6080ae
David Reed <reedswork@proton.me>
2023-02-27 19:43:10 -0500

    upgrade to v1.0.0-alpha.1

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -3,16 +3,13 @@ import baseConfig from "@reactea/core";
 import sassConfig from "@reactea/sass";
 import swConfig from "@reactea/sw";
 import tailwindConfig from "@reactea/tailwind";
-// types not included in v1.0.0-alpha
-import { isDevelopment } from "@reactea/config/consts";
 
 const reactea = createConfig();
 
 extendConfig(reactea, baseConfig());
 extendConfig(reactea, tailwindConfig());
 extendConfig(reactea, sassConfig());
-// workaround for v1.0.0-alpha
-if (!isDevelopment) extendConfig(reactea, await swConfig());
+extendConfig(reactea, await swConfig());
 
 const config = compileConfig(reactea);
 
